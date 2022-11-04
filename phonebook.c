8d596c82a03c9e9a34b6dc8a8b7fbf3e3a932312
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-11-04 18:47:35 -0400

    create custome data type

---
+++ phonebook.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+//create a custom data type
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
+int main(void)
+{
+    person people[2];
+
+    //initialize props
+    people[0].name = "Carter";
+    people[0].number = "+10-6530-4034";
+
+    people[1].name = "David";
+    people[1].number = "+10-33-44-5597";
+
+      for (int i = 0; i < 2; i++)
+    {
+
+        if(strcmp(people[i].name, "David") == 0)
+        {
+            printf("Found %s\n",people[i].number);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
